a736f25c203bdfbe7d16ad607bb68ef296602838
Vitor <vitorbastos195@gmail.com>
2025-03-19 14:19:40 -0300

    Reusing instantiation

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,13 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
-import java.util.Date;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        System.out.println("=== TEST 1: seller findById ===");
         Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
